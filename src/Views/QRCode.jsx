add2f084324a01fd798fc2f60e5af202352399cc
Faizan Khan <m.faisalkhan9@gmail.com>
2024-04-28 14:26:11 +0530

    a few changes

--- src/Views/QRCode.jsx
+++ src/Views/QRCode.jsx
@@ -3,75 +3,95 @@ import QRCode from 'qrcode.react';
 import Header from "./Header/Header";
 import { useLocation, useNavigate } from "react-router-dom";
 import { validate as uuidValidate } from 'uuid';
-import { auth } from './FireBase/Firebase';
+import { auth, db } from './FireBase/Firebase';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { CgSpinner } from 'react-icons/cg';
 
 const QRCodeComponent = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const uniqueString = new URLSearchParams(location.search).get('id');
+  const redirectUrl = `https://photobooth-nine-sable.vercel.app/redirect?id=${uniqueString}`;
 
-    const navigate = useNavigate();
+  const [validation, setValidation] = useState(null);
+  const [isQRCodeScanned, setIsQRCodeScanned] = useState(false);
+  const [photoUploaded, setPhotoUploaded] = useState(false);
+  const [photoLink, setPhotoLink] = useState('');
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            if (!user) {
-                navigate('/login');
-            } else {
-                console.log('User is signed in')
-            }
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setValidation(uuidValidate(uniqueString));
+
+        const userRef = doc(db, 'users', user.uid);
+        const unsubscribeFromSnapshot = onSnapshot(userRef, (doc) => {
+          const currentProcess = doc.data().CurrentProcess;
+
+          setIsQRCodeScanned(currentProcess.isQRCodeScanned);
+          setPhotoUploaded(currentProcess.photoUploaded);
+          setPhotoLink(currentProcess.photoLink || '');
         });
 
-        return unsubscribe;
-    }, [navigate]);
+        return () => {
+          unsubscribeFromSnapshot();
+        };
+      } else {
+        navigate('/login');
+      }
+    });
 
+    return unsubscribe;
+  }, [navigate, uniqueString]);
 
-    const [Validation, setValidation] = useState(true);
+  const handleGoBack = () => {
+    navigate("/Home");
+  };
 
-    const location = useLocation();
-    const uniqueString = new URLSearchParams(location.search).get('id');
-    const redirectUrl = `http://192.168.1.9:3000/redirect?id=${uniqueString}`;
-
-
-    useEffect(() => {
-        setValidation(uuidValidate(uniqueString));
-    }, [uniqueString])
-
-    const handleGoBack = () => {
-        navigate("/Home")
-    }
-
-    return (
-        <div className="flex flex-col bg-gray-200 min-h-screen">
-            <Header />
-
-            {Validation ?
-                (
-                    <main className="flex-1 flex flex-col items-center justify-center">
-                        <div className="w-96 h-96 bg-gray-300 rounded-lg flex items-center justify-center mb-4 relative">
-                            <QRCode
-                                value={redirectUrl}
-                                size={356}
-                            />
-                        </div>
-
-                        <h1>
-                            Please Scan the QR Code
-                        </h1>
-
-                        <div className="flex flex-col">
-                            <button className="mt-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                                onClick={handleGoBack}>
-                                Go Back
-                            </button>
-                        </div>
-                    </main>
-                ) : (
-                    <main className="flex-1 flex flex-col items-center justify-center">
-                        <h1>
-                            Where did you come from?
-                        </h1>
-
-                    </main>
-                )}
-        </div>
-    );
+  return (
+    <div className="flex flex-col bg-gray-200 min-h-screen">
+      <Header />
+      {validation === null ? (
+        <main className="flex-1 flex flex-col items-center justify-center">
+          <p>Loading...</p>
+        </main>
+      ) : validation ? (
+        <main className="flex-1 flex flex-col items-center justify-center">
+          {!isQRCodeScanned ? (
+            <>
+              <div className="w-96 h-96 bg-gray-300 rounded-lg flex items-center justify-center mb-4 relative">
+                <QRCode value={redirectUrl} size={356} />
+              </div>
+              <h1>Please Scan the QR Code</h1>
+            </>
+          ) : !photoUploaded ? (
+            <>
+              <CgSpinner size={64} className="mt-1 animate-spin" />
+              <p>Waiting for your picture upload to be completed</p>
+            </>
+          ) : (
+            <>
+              <img src={photoLink} alt="Uploaded" className="max-w-full max-h-96" />
+              <button className="mt-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                Processed with Photo
+              </button>
+            </>
+          )}
+          <div className="flex flex-col">
+            <button
+              className="mt-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleGoBack}
+            >
+              Go Back
+            </button>
+          </div>
+        </main>
+      ) : (
+        <main className="flex-1 flex flex-col items-center justify-center">
+          <h1>Where did you come from?</h1>
+        </main>
+      )}
+    </div>
+  );
 };
 
 export default QRCodeComponent;
